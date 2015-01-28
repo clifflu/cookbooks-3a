9208868f3b1c4644f0020f4e26bc72983fd4a4ad
Cliff Chao-kuan Lu <clifflu@gmail.com>
2015-01-28 18:26:36 +0800

    ...

--- dcs-3a/recipes/deploy.rb
+++ dcs-3a/recipes/deploy.rb
@@ -1,5 +1,17 @@
 include_recipe 'deploy'
 
+directory node[:nginx][:dir] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+directory node[:nginx][:log_dir] do
+  mode 0755
+  owner node[:nginx][:user]
+  action :create
+end
+
 node[:deploy].each do |application, deploy|
   Chef::Log.info("App: #{application}, Deploy: #{deploy}")
 end
